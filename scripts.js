473e5f7856beba333358e88ffa18a8811e7fa9dc
willyanne11 <willyannemayara5@gmail.com>
2024-12-02 20:39:00 -0300

    scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+ let numero = parseFloat(prompt("Digite um numero:"));
+
+    if (numero % 2 === 0) {
+      
+        alert('Voce digitou um numero par');
+    } else {
+
+        alert('Voce digitou um número impar');
+    }
